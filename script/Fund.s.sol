cae8c04a66853e3e87c123cd16b6708c974974e2
Pranabindu <you@example.com>
2025-11-18 05:53:48 -0800

    initial commit

---
+++ script/Fund.s.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.0;
+import "forge-std/Script.sol";
+contract Fund is Script {
+    function run() external payable {
+        // Nothing – just receive ETH when running with --broadcast and --private-key
+    }
+}
